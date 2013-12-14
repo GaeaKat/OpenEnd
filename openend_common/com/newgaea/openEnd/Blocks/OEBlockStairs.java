6c83edf3a913a3a9319b83a9202113474182bd63
nekosune <kat.swales@nekokittygames.com>
2013-12-14 01:49:13 +0000

    Cleaned up even more, and bed time

--- openend_common/com/newgaea/openEnd/Blocks/OEBlockStairs.java
+++ openend_common/com/newgaea/openEnd/Blocks/OEBlockStairs.java
@@ -10,10 +10,6 @@ public class OEBlockStairs extends BlockStairs
     public OEBlockStairs(int par1, Block par2Block, int par3)
     {
     	super(par1,par2Block,par3);
-        this.setHardness(par2Block.blockHardness);
-        this.setResistance(par2Block.blockResistance / 3.0F);
-        this.setStepSound(par2Block.stepSound);
-        this.setLightOpacity(255);
     }
 
    
